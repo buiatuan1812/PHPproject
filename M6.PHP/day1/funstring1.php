c4145456404915cc4b6736b7009302363a9546f9
buiatuan1812 <anhtuan1812@gmail.com>
2019-05-15 20:53:16 +0700

    111

--- M6.PHP/day1/funstring1.php
+++ M6.PHP/day1/funstring1.php
@@ -5,6 +5,28 @@
     <title>Title</title>
 </head>
 <body>
+<h1>Các hàm xử lý chuỗi trong php</h1>
+        <pre>
+            - strlen() đếm số kí tự trong 1 chuỗi
+            - str_word_count() đếm số từ trong 1 chuỗi
+            - strpos() tìm kiếm sự có mặt của 1 chuỗi con trong 1 chuỗi mẹ sẽ trả về false
+            nếu không tìm đc
+            trả về vị trí bắt đầu của chuỗi con trong chuỗi mẹ
+            - str_replace() thay thế chuỗi 1 bằng chuỗi 2 trong 1 chuỗi mẹ
+            - var_dump($bien) : hàm này được dùng để debug xem cấu trúc của biên thuộc kiểu
+                dữ liệu gì và chứa giá trị nào
+        </pre>
+
+    <?php
+    $find1 = strpos("thanh pho ha noi chao mung quy khach", "ha noi");
+    var_dump($find1);
+    echo "<br>";
+    $find1 = strpos("thanh pho ha noi chao mung quy khach", "da nang");
+    var_dump($find1);
+
+    ?>
+
+
 
 </body>
 </html>
