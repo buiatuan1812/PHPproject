91ef5ae486ea7805a7c37d4c25838949d5e4ffbe
buiatuan1812 <anhtuan1812@gmail.com>
2019-05-15 19:18:18 +0700

    111

---
+++ M6.PHP/day1/index2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h3>in ra chuỗi trên màn hình bằng PHP</h3>
+    <?php echo "hello php"; ?>
+    <h3>In ra một chuỗi các bạn có thể sử dụng '' hay "" để chứa chuỗi đó</h3>
+    <?php echo ' hello php'; ?>
+    <
+</body>
+</html>
