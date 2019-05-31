2e2a980b6677a674c9f9f8532864d6376636231b
buiatuan1812 <anhtuan1812@gmail.com>
2019-05-31 20:09:08 +0700

    111

---
+++ M6.PHP/day3/index2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <pre>
+        cú pháp lệnh switch case
+        n là giá trị của biến hoặc là biến
+
+        switch(n) {
+            case label1:
+    </pre>
+</body>
+</html>
